6a31c023d195753407c8ec8d11c4cdcd88c1b0eb
Siddharth <Siddharthranjan092@gmail.com>
2023-07-09 22:07:34 +0530

    added register page

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,8 +1,20 @@
 import React from "react"
+import "./Register.css"
 
 const Register = () => {
     return(
-        <>hello from Register</>
+        <>
+        <div className="register">
+            <h1>Register</h1>
+            <input type= "text" placeholder="Enter Your Name"/>
+            <input type= "text" placeholder="Enter Your Email"/>
+            <input type= "password" placeholder="Enter Your Password"/>
+            <input type= "password" placeholder="Enter Your Re-enter Password"/>
+            <div className="button">Register</div>
+            <div>or</div>
+            <div className="button">Login</div>
+        </div>
+        </>
     )
 }
 
