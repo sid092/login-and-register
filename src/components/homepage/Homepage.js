58302ad007761a137b23712bb3a4301e45a44074
Siddharth <Siddharthranjan092@gmail.com>
2023-07-09 19:04:20 +0530

    homepage complete

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './Homepage.css'
+
+const Homepage = () => {
+    return(
+        <>
+        <div className="homepage">
+            <h1>Hello Homepage</h1>
+            <div className="button">Logout</div>
+        </div>
+        </>
+    )
+}
+export default Homepage
---
+++ src/components/register/Register.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Register = () => {
+    return(
+        <>hello from Register</>
+    )
+}
+
+export default Register
